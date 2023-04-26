a32857e8a87384e09894c2ca3137e56eea42ca5f
Ola <hollabodey.kilaso@gmail.com>
2023-04-27 00:40:13 +0100

    Made changes to the html, css and javascript file

--- script.js
+++ script.js
@@ -2,10 +2,3 @@ const faq = document.querySelector('.accQuestion');
 const answer = document.querySelector('.accAnswer');
 
 
-faq.forEach( (faqs) => {
-    faqs.addEventListener('click', () => {
-        answer.classList.toggle('visibility');
-    })
-})
-
-
