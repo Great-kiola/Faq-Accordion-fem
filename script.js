91452a606c810c04baaa81ee45d1f2bff64f93c7
Ola <hollabodey.kilaso@gmail.com>
2023-04-27 06:40:49 +0100

    Made changes to the html and css file

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-const faq = document.querySelector('.accQuestion');
-const answer = document.querySelector('.accAnswer');
+const faq = document.querySelectorAll('.acc');
+let active = document.querySelectorAll('.acc.active');
 
+
+faq.forEach(faqs => {
+    faqs.addEventListener('click', () => {
+        faqs.classList.toggle('active');
+    })
+})
+
+active.forEach(actives => {
+    actives.addEventListener('click', () => {
+        actives.classList.remove('active');
+        actives.nextElementSibling.style.maxHeight = null;
+    });
+
+})
+
+// for(let j = 0; j < active.length; j++){
+//     active[j].classList.remove("active");
+//     active[j].nextElementSibling.style.maxHeight = null; //or 0px
+// }
+
+// faq.addEventListener('click', () => {
+//     answer.classList.toggle('visibility');
+//     icon.classList.toggle('active');
+    
+// })
 
