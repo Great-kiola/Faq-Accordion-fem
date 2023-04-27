2df45ef0c853206fedcc78146d950ef473639ce9
Ola <hollabodey.kilaso@gmail.com>
2023-04-27 13:51:42 +0100

    made changes to the js file

--- script.js
+++ script.js
@@ -1,29 +1,19 @@
-const faq = document.querySelectorAll('.acc');
-let active = document.querySelectorAll('.acc.active');
+const faq = document.querySelectorAll(".acc");
 
+faq.forEach((faqs) => {
+  faqs.addEventListener("click", () => {
+    const currentActive = document.querySelector(".acc.active");
+    if (currentActive && currentActive !== faqs) {
+      currentActive.classList.remove("active");
+      currentActive.lastElementChild.style.maxHeight = 0;
+    }
 
-faq.forEach(faqs => {
-    faqs.addEventListener('click', () => {
-        faqs.classList.toggle('active');
-    })
-})
+    faqs.classList.toggle("active");
 
-active.forEach(actives => {
-    actives.addEventListener('click', () => {
-        actives.classList.remove('active');
-        actives.nextElementSibling.style.maxHeight = null;
-    });
+    const accAnswer = faqs.lastElementChild;
+    const height = faqs.classList.contains("active")
 
-})
-
-// for(let j = 0; j < active.length; j++){
-//     active[j].classList.remove("active");
-//     active[j].nextElementSibling.style.maxHeight = null; //or 0px
-// }
-
-// faq.addEventListener('click', () => {
-//     answer.classList.toggle('visibility');
-//     icon.classList.toggle('active');
-    
-// })
-
+    ? accAnswer.scrollHeight : 0;
+    accAnswer.style.maxHeight = `${height}px`;
+  });
+});
